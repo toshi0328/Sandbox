cce3981aee2479ffcc25a3ec6a99da4143b604c4
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-09-27 17:30:38 +0900

    add ror auth test

---
+++ ror_auth/auth_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthTest::Application.config.secret_token = '43615938b60dc832a999f4cbc3de12150064cc9d13681ffdf4d3c1fddce19ca958f5c8508010aff802b6c7aa5d11f976ba8e874e839fa5bb4e79c17e343bc160'
