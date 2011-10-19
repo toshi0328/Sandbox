0a8e962b85e1154fc172af99322518c0afb657eb
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-10-19 09:10:24 +0900

    changed test_line.rb

---
+++ openGLRuby/test/test_line.rb
@@ -0,0 +1,8 @@
+require 'gmath3D'
+require 'disp3D'
+
+main_view = Disp3D::View.new(100,100,200,200)
+line_geom = GMath3D::FiniteLine.new()
+line_node = Disp3D::NodeLines.new(line_geom)
+main_view.world_scene_graph.add(line_node)
+main_view.start
